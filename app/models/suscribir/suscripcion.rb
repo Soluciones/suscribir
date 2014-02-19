098b84728a16d9bc3a7ab620de08f1614f728223
esaborit4code <edu@emergia.net>
2014-02-19 18:19:10 +0100

    Validations for Suscripcion

--- app/models/suscribir/suscripcion.rb
+++ app/models/suscribir/suscripcion.rb
@@ -1,4 +1,9 @@
+# coding: UTF-8
+
 module Suscribir
   class Suscripcion < ActiveRecord::Base
+    validates :suscribible_type, :suscribible_id, presence: true
+    validates :dominio_de_alta, presence: true
+    validates :email, presence: true, uniqueness: { scope: [:suscribible_type, :suscribible_id, :dominio_de_alta] }
   end
 end
