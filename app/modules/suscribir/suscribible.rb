195766acdb0acdc06c169bbdfa0389da801e7e65
esaborit4code <edu@emergia.net>
2014-02-19 19:33:23 +0100

    Created Suscribible concern

---
+++ app/modules/suscribir/suscribible.rb
@@ -0,0 +1,11 @@
+# coding: UTF-8
+
+require 'active_support/concern'
+
+module Suscribir::Suscribible
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :suscripciones, as: :suscribible , class_name: 'Suscribir::Suscripcion'
+  end
+end
