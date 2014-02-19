b2521912600205710c3e5bd78f05a992743db6c9
esaborit4code <edu@emergia.net>
2014-02-19 19:32:37 +0100

    Suscripcion belongs to Suscribible and Suscriptor

--- app/models/suscribir/suscripcion.rb
+++ app/models/suscribir/suscripcion.rb
@@ -2,6 +2,9 @@
 
 module Suscribir
   class Suscripcion < ActiveRecord::Base
+    belongs_to :suscribible, polymorphic: true
+    belongs_to :suscriptor, polymorphic: true
+
     validates :suscribible_type, :suscribible_id, presence: true
     validates :dominio_de_alta, presence: true
     validates :email, presence: true, uniqueness: { scope: [:suscribible_type, :suscribible_id, :dominio_de_alta] }
