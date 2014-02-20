ac752014de5f5839cc9c1f469c50b7b50fbaa212
esaborit4code <edu@emergia.net>
2014-02-20 12:21:42 +0100

    Dummy's Usuario should implement the nombre_apellidos method

--- test/dummy/app/models/usuario.rb
+++ test/dummy/app/models/usuario.rb
@@ -2,4 +2,14 @@
 
 class Usuario < ActiveRecord::Base
   include Suscribir::Suscriptor
+
+  @nombre_apellidos = nil
+
+  def nombre_apellidos
+    @nombre_apellidos ||= "#{nombre} #{apellidos}"
+  end
+
+  def nombre_apellidos=(value)
+    @nombre_apellidos = value
+  end
 end
