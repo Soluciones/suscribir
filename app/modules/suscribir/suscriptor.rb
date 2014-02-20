c6526488e6d1575cdffa9dbedad1f6153e951c34
esaborit4code <edu@emergia.net>
2014-02-20 10:05:43 +0100

    First feature and test for Suscirptor

--- app/modules/suscribir/suscriptor.rb
+++ app/modules/suscribir/suscriptor.rb
@@ -8,4 +8,8 @@ module Suscribir::Suscriptor
   included do
     has_many :suscripciones, as: :suscriptor , class_name: 'Suscribir::Suscripcion'
   end
+
+  def busca_suscripcion(suscribible, dominio_de_alta)
+    suscripciones.where(suscribible_id: suscribible.id, suscribible_type: suscribible.class.model_name, dominio_de_alta: dominio_de_alta).first
+  end
 end
