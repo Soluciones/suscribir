516879b41fb73f9805fd45525fe1ce5e6fff8858
esaborit4code <edu@emergia.net>
2014-02-21 12:42:10 +0100

    I forgot to add the SuscripcionSetUserIfFoundObserver to Suscripcion

--- config/initializers/observers.rb
+++ config/initializers/observers.rb
@@ -1,3 +1,6 @@
 # coding: UTF-8
 
-Suscribir::Suscripcion.add_observer Suscribir::SuscripcionAddToSendGridObserver.new unless Rails.env.test?
+unless Rails.env.test?
+  Suscribir::Suscripcion.add_observer Suscribir::SuscripcionAddToSendGridObserver.new
+  Suscribir::Suscripcion.add_observer Suscribir::SuscripcionSetUserIfFoundObserver.new
+end
