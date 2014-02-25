c2f24ef03887f5338177dfb6d4814a92114e06eb
esaborit4code <edu@emergia.net>
2014-02-25 18:59:15 +0100

    Added Suscriptor#desuscribeme_de! and its tests

--- app/modules/suscribir/suscriptor.rb
+++ app/modules/suscribir/suscriptor.rb
@@ -16,4 +16,8 @@ module Suscribir::Suscriptor
   def suscribeme_a!(suscribible, dominio_de_alta = 'es')
     suscripciones.create(email: email, suscribible_id: suscribible.id, suscribible_type: suscribible.class.model_name, dominio_de_alta: dominio_de_alta)
   end
+
+  def desuscribeme_de!(suscribible, dominio_de_alta = 'es')
+    busca_suscripcion(suscribible, dominio_de_alta).destroy
+  end
 end
