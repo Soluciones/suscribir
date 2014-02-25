deafc54ed50fa846354138a18a1ac81caaf9d922
esaborit4code <edu@emergia.net>
2014-02-25 18:39:19 +0100

    Improved suscripcion factory

--- spec/factories/suscribir/suscripcion.rb
+++ spec/factories/suscribir/suscripcion.rb
@@ -3,5 +3,19 @@
 FactoryGirl.define do
   factory :suscripcion, class: Suscribir::Suscripcion do
     suscriptor
+    association :suscribible, factory: :tematica
+
+    factory :suscripcion_con_suscriptor do
+      association :suscriptor, factory: :usuario
+
+      after(:build) do |suscripcion|
+        suscriptor = suscripcion.suscriptor
+
+        suscripcion.email = suscriptor.email
+        suscripcion.nombre_apellidos = suscriptor.nombre_apellidos
+        suscripcion.cod_postal = suscriptor.cod_postal
+        suscripcion.provincia_id = suscriptor.provincia_id
+      end
+    end
   end
 end
