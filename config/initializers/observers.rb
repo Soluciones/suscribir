4fbad60dd9678b7a525aac4b2a1dfd2c359a1504
esaborit4code <edu@emergia.net>
2014-02-26 17:09:17 +0100

    Replacing SuscripcionAddToSendGridObserver and SuscripcionDeleteFromSendGridObserver by SuscripcionSyncSendGridObserver

--- config/initializers/observers.rb
+++ config/initializers/observers.rb
@@ -1,7 +1,6 @@
 # coding: UTF-8
 
 unless Rails.env.test?
-  Suscribir::SuscripcionMediator.add_observer Suscribir::SuscripcionAddToSendGridObserver.new
-  Suscribir::SuscripcionMediator.add_observer Suscribir::SuscripcionDeleteFromSendGridObserver.new
+  Suscribir::SuscripcionMediator.add_observer Suscribir::SuscripcionSyncSendGridObserver.new
   Suscribir::SuscripcionMediator.add_observer Suscribir::SuscripcionSetUserIfFoundObserver.new
 end
