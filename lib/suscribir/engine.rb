4d86fc562a0d7725ce4557b5152fe12acfadc828
esaborit4code <edu@emergia.net>
2014-04-29 19:48:22 +0200

    Avoiding observers override

--- lib/suscribir/engine.rb
+++ lib/suscribir/engine.rb
@@ -2,6 +2,9 @@ module Suscribir
   class Engine < Rails::Engine
     isolate_namespace Suscribir
 
-    config.active_record.observers = 'Suscribir::SuscripcionMediator'
+    # Para que la applicación arranque los observers hay que registralos evitando sobreescribir los que hayan sido
+    # asignados anteriormente por otras partes (por ejemplo, por otros engines)
+    config.active_record.observers = [config.active_record.observers].flatten.compact
+    config.active_record.observers << 'Suscribir::SuscripcionMediator'
   end
 end
