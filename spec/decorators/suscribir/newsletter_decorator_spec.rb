997cfbf470bf89c43005e0ccaa870aa215a60127
Fernan2 <fernandorankia@gmail.com>
2014-07-07 10:19:09 +0200

    Better explanation of what I'm testing

--- spec/decorators/suscribir/newsletter_decorator_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Suscribir::NewsletterDecorator do
-  describe 'param_key' do
-    it 'devuelve "newsletter"' do
-      expect(Suscribir::Newsletter.new.decorate.param_key).to eq('newsletter')
-    end
-  end
-end
