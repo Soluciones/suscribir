2ac089c98dbe5ad7374c987bea4fbc2e483824d4
Fernan2 <fernandorankia@gmail.com>
2015-01-28 13:50:23 +0100

    Test the controller methods

--- lib/suscribir/aplicacion.rb
+++
@@ -1,3 +0,0 @@
-def tokeniza_email(email)
-  Digest::SHA1.hexdigest("tokeniza_email_#{ Rails.application.secrets.esta_web_secret_token }_#{ email }")
-end
--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def pon_lateral
+
+  end
+
+  def render_404
+    render text: 'Not found', status: 404
+  end
 end
