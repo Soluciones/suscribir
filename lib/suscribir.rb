c908812980681614ced263d6b3d53686b1d6ed22
Fernan2 <fernandorankia@gmail.com>
2015-02-04 11:02:09 +0100

    New parameter for Suscribir::Personalizacion

--- lib/suscribir.rb
+++ lib/suscribir.rb
@@ -7,6 +7,6 @@ require 'gatling_gun'
 
 module Suscribir
   module Personalizacion
-    mattr_accessor :layout, :email_contacto
+    mattr_accessor :layout, :email_contacto, :email_contacto_en_confirmacion_baja
   end
 end
