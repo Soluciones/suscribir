9b877de6d6f8bd924262f84f05240b6cbe8b4df2
Fernan2 <fernandorankia@gmail.com>
2015-02-09 13:15:33 +0100

    Test & fix desuscribir

---
+++ spec/features/desuscribir_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'desuscribir' do
+  context 'con una suscripción ya existente' do
+    let(:suscribible) { create(:tematica) }
+    let(:suscripcion) { create(:suscripcion, suscribible: suscribible, suscriptor: build_stubbed(:usuario)) }
+
+    it 'muestra el enlace de desuscribir' do
+      visit "/suscribir/confirmar_baja/#{ suscripcion.id }/#{ suscripcion.token }"
+      expect(page.status_code).to be(200)
+    end
+  end
+end
