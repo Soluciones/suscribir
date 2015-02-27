0d500eef7bb62fee5c641c9a1df93a3fc74e2804
Fernan2 <fernandorankia@gmail.com>
2015-02-27 13:03:36 +0100

    unhounding

--- app/modules/suscribir/suscripcion_base.rb
+++ app/modules/suscribir/suscripcion_base.rb
@@ -34,7 +34,7 @@ module Suscribir
         atributos_del_suscribible = { suscribible: suscribible, dominio_de_alta: dominio_de_alta }
         atributos_de_la_suscripcion = atributos_del_suscriptor.merge(atributos_del_suscribible)
 
-        create(atributos_de_la_suscripcion) 
+        create(atributos_de_la_suscripcion)
       rescue ActiveRecord::RecordNotUnique # only on race conditions should happen...
         busca_suscripcion(suscriptor, suscribible, dominio_de_alta)
       end
