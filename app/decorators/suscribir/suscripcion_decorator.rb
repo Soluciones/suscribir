b6af2796ceacacadf3e649d394a125f344410366
Fernan2 <fernandorankia@gmail.com>
2015-03-11 19:36:19 +0100

    That nasty code is the only one that works on a cron task...

--- app/decorators/suscribir/suscripcion_decorator.rb
+++ app/decorators/suscribir/suscripcion_decorator.rb
@@ -3,7 +3,10 @@ module Suscribir
     delegate_all
 
     def url_desuscribir
-      h.engine_suscribir.pedir_confirmacion_baja_url(id, token)
+      "#{ HTTP_DOMINIOS[dominio_de_alta.to_sym] }/baja/#{ id }/#{ token }"
+      # SI QUIERES INTENTAR HACERLO BIEN CON
+      # h.engine_suscribir.pedir_confirmacion_baja_url(id, token, host: mi_dominio)
+      # ASEGURATE DE QUE TE FUNCIONE DESDE EL CRON
     end
   end
 end
