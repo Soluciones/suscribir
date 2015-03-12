38bf91372aa1eb9d0ca84d13a479b3711470ef4b
Fernan2 <fernandorankia@gmail.com>
2015-03-12 12:37:46 +0100

    We wanted the GET route for deleting (asking confirmation), not the DELETE one (executing)

--- app/decorators/suscribir/suscripcion_decorator.rb
+++ app/decorators/suscribir/suscripcion_decorator.rb
@@ -3,7 +3,7 @@ module Suscribir
     delegate_all
 
     def url_desuscribir
-      "#{ HTTP_DOMINIOS[dominio_de_alta.to_sym] }/suscribir/baja/#{ id }/#{ token }"
+      "#{ HTTP_DOMINIOS[dominio_de_alta.to_sym] }/suscribir/confirmar_baja/#{ id }/#{ token }"
       # SI QUIERES INTENTAR HACERLO BIEN CON
       # h.engine_suscribir.pedir_confirmacion_baja_url(id, token, host: mi_dominio)
       # ASEGURATE DE QUE TE FUNCIONE DESDE EL CRON
