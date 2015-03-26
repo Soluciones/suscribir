2c9cb117710c37ef3699120201f7a6b091520296
David BF <burretfresket@gmail.com>
2015-03-26 10:48:52 +0100

    Suscriptor.suscripciones should be removed instead of nullified

--- app/modules/suscribir/suscriptor.rb
+++ app/modules/suscribir/suscriptor.rb
@@ -6,7 +6,7 @@ module Suscribir::Suscriptor
   extend ActiveSupport::Concern
 
   included do
-    has_many :suscripciones, as: :suscriptor , class_name: 'Suscribir::Suscripcion'
+    has_many :suscripciones, as: :suscriptor, class_name: 'Suscribir::Suscripcion', dependent: :delete_all
   end
 
   def busca_suscripcion(suscribible, dominio_de_alta)
