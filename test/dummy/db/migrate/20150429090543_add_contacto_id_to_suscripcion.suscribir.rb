c0472a94dcc41390dd268a7b22c20aa89598f56a
David BF <burretfresket@gmail.com>
2015-06-08 14:35:44 +0200

    Delete duplicated migrations

--- test/dummy/db/migrate/20150429090543_add_contacto_id_to_suscripcion.suscribir.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from suscribir (originally 20150429090008)
-class AddContactoIdToSuscripcion < ActiveRecord::Migration
-  def change
-    add_column :suscribir_suscripciones, :contacto_id, :integer
-    add_index :suscribir_suscripciones, :contacto_id
-  end
-end
