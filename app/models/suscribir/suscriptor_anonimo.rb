6c97eec8919a444514f7d1472991604c8bc836e0
Fernan2 <fernandorankia@gmail.com>
2015-06-18 19:20:07 +0200

    SuscriptorAnonimo can be initialized with data other than email

--- app/models/suscribir/suscriptor_anonimo.rb
+++ app/models/suscribir/suscriptor_anonimo.rb
@@ -4,8 +4,11 @@ module Suscribir
   class SuscriptorAnonimo
     attr_accessor :email, :nombre_apellidos, :cod_postal, :provincia_id
 
-    def initialize(email = nil)
-      self.email = email
+    def initialize(campos = {})
+      self.email = campos[:email]
+      self.nombre_apellidos = campos[:nombre_apellidos]
+      self.cod_postal = campos[:cod_postal]
+      self.provincia_id = campos[:provincia_id]
     end
 
     def suscribeme_a!(suscribible, dominio_de_alta = 'es')
