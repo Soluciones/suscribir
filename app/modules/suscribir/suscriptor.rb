758d13fc21f1ff9f26cf53c11abde7555969689c
lvargas <lvargas@rankia.com>
2015-06-26 14:31:46 +0200

    Usefull methods to clean rankia & verema

--- app/modules/suscribir/suscriptor.rb
+++ app/modules/suscribir/suscriptor.rb
@@ -22,4 +22,32 @@ module Suscribir::Suscriptor
   def desuscribeme_de!(suscribible, dominio_de_alta = 'es')
     Suscribir::Suscripcion.desuscribir(self, suscribible, dominio_de_alta)
   end
+
+  def suscrito_a?(suscribible)
+    suscripciones.exists?(suscribible_id: suscribible.id, suscribible_type: suscribible.class.to_s)
+  end
+
+  def actualiza_suscripciones(clase, ids_seleccionadas)
+    ids_ya_suscritas = ids_ya_suscritas(clase)
+    suscribe_a_nuevas(clase, ids_seleccionadas, ids_ya_suscritas)
+    desuscribe_ya_no_quiere(clase, ids_seleccionadas, ids_ya_suscritas)
+  end
+
+  def ids_ya_suscritas(tipo)
+    suscripciones.where(suscribible_type: tipo, dominio_de_alta: I18n.locale).pluck(:suscribible_id)
+  end
+
+  private
+
+  def suscribe_a_nuevas(clase_suscribible, ids_seleccionadas, ids_ya_suscritas)
+    ids_nuevas = ids_seleccionadas.select { |id| ids_ya_suscritas.exclude? id }
+    return if ids_nuevas.blank?
+    suscribeme_a!(clase_suscribible.where(id: ids_nuevas), I18n.locale)
+  end
+
+  def desuscribe_ya_no_quiere(clase_suscribible, ids_seleccionadas, ids_ya_suscritas)
+    ids_que_ya_no_quiere = ids_ya_suscritas.select { |id| ids_seleccionadas.exclude? id }
+    return if ids_que_ya_no_quiere.blank?
+    desuscribeme_de!(clase_suscribible.where(id: ids_que_ya_no_quiere), I18n.locale)
+  end
 end
